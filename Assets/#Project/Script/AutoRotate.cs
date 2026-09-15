4e5da28971662234daca6450715f7890caabb0da
moulay-belbachir-vinci <moulay.belbachir@student.vinci.be>
2026-09-15 17:44:35 +0200

    end of s1

---
+++ Assets/#Project/Script/AutoRotate.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+
+    [SerializeField] float rotationSpeed;
+
+    [HideInInspector] public bool test;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/#Project/Script/DirectInput.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DirectInput : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Keyboard keyboard = Keyboard.current;
+        if( keyboard == null) return;
+
+
+        if (keyboard.spaceKey.wasPressedThisFrame)
+        {
+            Debug.Log("Hello world !");
+        }
+
+        if (keyboard.rightArrowKey.isPressed)
+        {
+            transform.position += Vector3.right * 5 * Time.deltaTime;
+        }
+
+        if (keyboard.leftArrowKey.isPressed)
+        {
+            transform.position -= Vector3.right * 5 * Time.deltaTime;
+        }
+
+    }
+}
